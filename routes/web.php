8fe1990a21fc4229be3992a31ed0977f58acae17
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-10-22 20:59:16 +0700

    project page

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,13 @@ Route::redirect('/', '/dashboard');
 // });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', fn() => inertia('Dashboard'))->name('dashboard');
 
     Route::resource('project', ProjectController::class);
     Route::resource('task', TaskController::class);
     Route::resource('user', UserController::class);
+
+    // Route::get('/project', fn() => Inertia::render('Project/index'));
 });
 
 
